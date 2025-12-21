796e3501a3b5c25afc74c54aa19af673ef717eae
parekhprachi410 <parekhprachi410@gmail.com>
2025-12-22 01:38:49 +0530

    lstm_inference.h

--- lstm_inference.h
+++ lstm_inference.h
@@ -3,19 +3,16 @@
 
 #include <stddef.h>
 
-// LSTM inference functions for ESP32
 typedef struct {
     float* hidden_state;
     float* cell_state;
 } LSTMState;
 
-// Initialize LSTM inference
 void lstm_init(void);
 
-// Single prediction function
 float predict_temperature(const float* input_sequence);
 
-// Cleanup function
 void lstm_cleanup(void);
+
 
 #endif
